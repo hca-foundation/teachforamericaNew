c888cd5229a3a93adfb3829ffc4b1f359583d0bc
Alex <rollinglex@gmail.com>
2020-11-15 02:15:30 -0600

    code clean up

--- twilio-serverless/handler.js
+++ twilio-serverless/handler.js
@@ -3,8 +3,7 @@ const Messenger = require('./messenger.js')
 const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID
 const twilioAuthToken = process.env.TWILIO_AUTH_TOKEN
 const twilioClient = require('twilio')(twilioAccountSid, twilioAuthToken) // eslint-disable-line
-//////////////
-//////////////
+
 const service = twilioClient.notify.services(
   process.env.TWILIO_NOTIFY_SERVICE_SID
 )
@@ -15,7 +14,7 @@ module.exports.sendText = (event, context, callback) => {
 
   console.log('context', context)
   console.log('event', event)
-  //{ to: '+16154897861', message: 'From the application' },
+
   const response = {
     headers: { 'Access-Control-Allow-Origin': '*' }, // CORS requirement
     statusCode: 200
--- twilio-serverless/messenger.js
+++ twilio-serverless/messenger.js
@@ -1,5 +1,4 @@
 'use strict'
-//https://www.twilio.com/blog/2017/12/send-bulk-sms-twilio-node-js.html
 
 class Messenger {
   constructor (client, body, service) {
@@ -8,13 +7,7 @@ class Messenger {
     this.service = service
   }
 
-  send (event) {
-    // use twilio SDK to send text message
-    // const sms = {
-    //   to: this.body.to,
-    //   body: this.body.message,
-    //   from: '+16156221017'
-    // }
+  send () {
     const { phoneNumbers } = this.body
     console.log('phoneNumbers', phoneNumbers)
     //limit of 10,000 numbers to send.  split request to send more
@@ -35,24 +28,8 @@ class Messenger {
       .catch(err => {
         console.error(err)
       })
-    // Promise.all(
-    //   numbers.map(number => {
-    //     return twilio.messages.create({
-    //       to: number,
-    //       from: process.env.TWILIO_MESSAGING_SERVICE_SID,
-    //       body: body
-    //     })
-    //   })
-    // )
-    //   .then(messages => {
-    //     console.log('Messages sent!: ', messages)
-    //   })
-    //   .catch(err => console.error(err))
-    ////
 
     // sms.mediaUrl = 'https://live.staticflickr.com/5814/20058976494_0e6dc4caaf_q.jpg'
-
-    // return this.client.messages.create(sms)
   }
 }
 
